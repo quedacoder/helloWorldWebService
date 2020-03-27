f1c7909f802071dc618f1198fe35b29b0a0f2af3
quedacoder <quedacoder@gmail.com>
2020-03-26 23:35:01 -0500

    initial commit

---
+++ src/main/java/com/quedacoder/ws/soap/HelloWS.java
@@ -0,0 +1,17 @@
+package com.quedacoder.ws.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import org.apache.cxf.feature.Features;
+
+@WebService
+@Features(features="org.apache.cxf.feature.LoggingFeature")
+public class HelloWS {
+	
+	@WebMethod
+	public String hello() {
+		return "Hello World";
+	}
+
+}
